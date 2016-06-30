029bfb5e63ba974df411e7e76a2ff1c166b4b75e
Ken Salanio <kssalanio@gmail.com>
2016-06-30 17:39:39 +0800

    added from_date optional arg for finding fmc_dtms

--- preprocess_data/remote/tiling/utils.py
+++ preprocess_data/remote/tiling/utils.py
@@ -1,4 +1,4 @@
-import os
+import os, subprocess
 import osgeotools
 import math
 import argparse
@@ -13,43 +13,71 @@ def _floor(x):
 def _ceil(x):
     return int(math.ceil(x / float(_TILE_SIZE)) * _TILE_SIZE)
 
-def _parse_arguments():
-    # Parse arguments
-    parser = argparse.ArgumentParser(description="Finds \
-tiles from input DEM.",
-                                     epilog="Example: ./tile_dem.py \
--d data/MINDANAO1/d_mdn/ -t dsm -p WGS_84_UTM_zone_51N.prj -o output/")
-    parser.add_argument("--version", action="version",
-                        version=_version)
-    parser.add_argument("-v", "--verbose", action="count")
-    parser.add_argument("-d", "--dem", required=True,
-                        help="Path to the DEM directory/file.")
-    parser.add_argument("-t", "--type", choices=["dsm", "dtm"], required=True,
-                        help="If DEM is a DSM/DTM.")
-    parser.add_argument("-p", "--prj_file", required=True,
-                        help="Path to the projection file. Checks if the DEM's \
-projection is the same.")
-    parser.add_argument("-o", "--output-dir", required=True,
-                        help="Path to output directory.")
-    args = parser.parse_args()
-    return args
+def get_dir_size(dir_path = '.'):
+    cli_cmd="du -s "+dir_path
+    p = subprocess.Popen(cli_cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    return out.split('\t')[0]
 
-
-def find_fmc_dtms(toplevel_dir, output_log_file, prj_file):
+def find_fmc_dtms(toplevel_dir, output_log_file, prj_file, from_date=None):
+    from_dt = None
+    if from_date is not None:
+        from_dt = datetime.strptime(from_date, "%Y/%m/%d")
     with open(output_log_file, 'w') as log_fh:
         for dem_dir, dirs, files in os.walk(toplevel_dir):
             for name in files:
-                if name == "hdr.adf":
-                    try:
-                        dem = osgeotools.open_raster(dem_dir, prj_file)
-                        tile_extents = [_floor(dem["extents"]["min_x"]),
-                                        _floor(dem["extents"]["min_y"]),
-                                        _ceil(dem["extents"]["max_x"]),
-                                        _ceil(dem["extents"]["max_y"])]
+                time_created = datetime.fromtimestamp(os.path.getctime(dem_dir))
+                if from_date is None or time_created > from_dt:
+                    if name == "hdr.adf":
                         time_created = datetime.fromtimestamp(os.path.getctime(dem_dir)).strftime("%Y/%m/%d")
-                        log_fh.write("{0}, {1}, {2}, None\n".format(time_created, dem_dir, tile_extents))
-                    except Exception as e:
-                        log_fh.write("{0}, {1} ERROR, {2}\n".format(time_created, dem_dir, e))
+                        time_modded = datetime.fromtimestamp(os.path.getmtime(dem_dir)).strftime("%Y/%m/%d")
+                        #print "[{0}] -  {1}, None".format(time_created, dem_dir)
+                        print "[{0} || {1}] -  {2}, None".format(time_created, time_modded, os.path.join(dem_dir,name))
+                        try:
+                            dem = osgeotools.open_raster(dem_dir, prj_file)
+                            tile_extents = [_floor(dem["extents"]["min_x"]),
+                                            _floor(dem["extents"]["min_y"]),
+                                            _ceil(dem["extents"]["max_x"]),
+                                            _ceil(dem["extents"]["max_y"])]
+                            time_created = datetime.fromtimestamp(os.path.getctime(dem_dir)).strftime("%Y/%m/%d")
+                            status ="NEW"
+                            rb_name = dem_dir.split(os.sep)[8]
+                            log_fh.write("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, None\n".format(time_created, 
+                                                                                                        dem_dir, 
+                                                                                                        status, 
+                                                                                                        rb_name, 
+                                                                                                        get_dir_size(dem_dir), 
+                                                                                                        tile_extents[0], tile_extents[1], tile_extents[2], tile_extents[3]))
+                        except Exception as e:
+                            #log_fh.write("{0}, {1} ERROR, {2}\n".format(time_created, dem_dir, e))
+                            status ="ERROR"
+                            log_fh.write("{0}, {1}, {2}, {3}, {4}, , , , , {5}\n".format(time_created, 
+                                                                                            dem_dir, 
+                                                                                            status, 
+                                                                                            rb_name, 
+                                                                                            get_dir_size(dem_dir), 
+                                                                                            e))
+
+def test_find_fmc_dtm(toplevel_dir, from_date):
+    for dem_dir, dirs, files in os.walk(toplevel_dir):
+        for name in files:
+            if name == "hdr.adf":
+                #time_created = datetime.fromtimestamp(os.path.getctime(os.path.join(dem_dir,name))).strftime("%Y/%m/%d")
+                #time_created = datetime.fromtimestamp(os.path.getctime(os.path.join(dem_dir,name)))
+                time_created = datetime.fromtimestamp(os.path.getctime(dem_dir))
+                from_dt = datetime.strptime(from_date, "%Y/%m/%d")
+                if time_created > from_dt:
+                    print "[{0}] -  {1}, None\n".format(time_created, os.path.join(dem_dir))
+
+"""
+toplevel_dir="/mnt/geostorage/EXCHANGE/DPC/MISCELLANEOUS/For_FMC"
+output_log_file="/home/AD/autotiler/fmc_dtms_20160603.log"
+prj_file="/home/AD/autotiler/scripts/tile_dem/WGS_84_UTM_zone_51N.prj"
+from_date="2016/06/02"
+
+find_fmc_dtms(toplevel_dir, output_log_file, prj_file, from_date)
+
+"""
 
 if __name__ == '__main__':
-    args = _parse_arguments()
+    pass
