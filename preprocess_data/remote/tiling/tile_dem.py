1dfe9dd9e9e12f40356064ba68b59f2930179241
Ken Salanio <kssalanio@gmail.com>
2016-10-17 11:52:37 +0800

    added function to be called by main automation script

--- preprocess_data/remote/tiling/tile_dem.py
+++ preprocess_data/remote/tiling/tile_dem.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python2.7
-
 import argparse
 import logging
 import math
@@ -42,6 +40,28 @@ def _setup_logging(args):
     fh.setFormatter(formatter)
     _logger.addHandler(fh)
 
+def _setup_logging_2(logfile,verbosity=None):
+    # Setup logging
+    _logger.setLevel(_LOG_LEVEL)
+    formatter = logging.Formatter("[%(asctime)s] %(filename)s \
+(%(levelname)s,%(lineno)d) : %(message)s")
+
+    # Check verbosity for console
+    if verbosity and verbosity >= 1:
+        global _CONS_LOG_LEVEL
+        _CONS_LOG_LEVEL = logging.DEBUG
+
+    # Setup console logging
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(_CONS_LOG_LEVEL)
+    ch.setFormatter(formatter)
+    _logger.addHandler(ch)
+
+    # Setup file logging
+    fh = logging.FileHandler(logfile)
+    fh.setLevel(_FILE_LOG_LEVEL)
+    fh.setFormatter(formatter)
+    _logger.addHandler(fh)
 
 def _parse_arguments():
     # Parse arguments
@@ -77,6 +97,138 @@ def _floor(x):
 
 def _ceil(x):
     return int(math.ceil(x / float(_TILE_SIZE)) * _TILE_SIZE)
+
+"""
+    <src_dem>   - location of source DEM to be tiled
+    <out_dir>   - directory to contained the tiled files
+    <dem_type>  - DTM or DSM
+    <prj_file>  - .prj file for added parameters
+    <temp_dir>  - temporary directory to store reporjected/resampled raster
+    <log_file>  - log file output
+    <_logger>   - python logger instance
+"""
+def tile_dem(src_dem,out_dir,dem_type,rbid,prj_file,temp_dir,log_file,_logger=_logger):
+    _setup_logging_2(log_file)
+    # Open DEM raster
+    try:
+        dem = osgeotools.open_raster(src_dem, prj_file)
+    except osgeotools.ProjectionDoesNotMatchError:
+        _logger.info('Projection from %s does not match raster dataset!',
+                     prj_file)
+        _logger.info('Reprojecting DEM...')
+        reprojected_dem_path = osgeotools.get_tempfile(temp_dir) + '.tif'
+        try:
+            gdalwarp = subprocess.check_output(['gdalwarp', '-t_srs',
+                                                os.path.abspath(prj_file),
+                                                os.path.abspath(src_dem),
+                                                reprojected_dem_path])
+            _logger.info('Reprojecting DEM...Done!')
+        except subprocess.CalledProcessError:
+            _logger.exception('Error reprojecting DEM! Exiting.')
+            exit(1)
+        # Open new DEM
+        dem = osgeotools.open_raster(reprojected_dem_path, prj_file)
+        # exit(1)
+
+    _logger.info("Current DEM geotransform:\n%s", dem["geotransform"])
+    _logger.info("Current DEM extents:\n%s", dem["extents"])
+
+    # Get tile extents
+    # Basically, the extents (to the nearest 1k) of the bounding box that
+    # encompasses the raster DEM.
+    tile_extents = {"min_x": _floor(dem["extents"]["min_x"]),
+                    "max_x": _ceil(dem["extents"]["max_x"]),
+                    "min_y": _floor(dem["extents"]["min_y"]),
+                    "max_y": _ceil(dem["extents"]["max_y"])}
+    _logger.info("Tile extents:\n%s", tile_extents)
+
+    # Resample DEM to tile extents
+    _logger.info("Resampling image...")
+    # Get a temporary file for resampled raster
+    # random_string = ''.join(random.choice(string.ascii_lowercase +
+    #                                       string.digits) for _ in range(16))
+    # temp_dir = osgeotools.isexists(temp_dir)
+    # resampled_dem_path = os.path.join(temp_dir,
+    #                                   "tile_dem_tmp_" + random_string)
+    resampled_dem_path = osgeotools.get_tempfile(temp_dir)
+    _logger.debug("resample_raster = %s", resampled_dem_path)
+    osgeotools.resample_raster(dem, tile_extents, resampled_dem_path)
+
+    # Open resampled DEM
+    resampled_dem = osgeotools.open_raster(resampled_dem_path, prj_file)
+    _logger.info("Resampled DEM geotransform:\n%s",
+                 resampled_dem["geotransform"])
+    _logger.info("Resampled DEM extents:\n%s", resampled_dem["extents"])
+
+    # Compare rasters
+    _logger.info("Comparing rasters...")
+    avg, std = osgeotools.compare_rasters(dem, resampled_dem)
+    _logger.info("Average difference: %s", avg)
+    _logger.info("Standard deviation: %s", std)
+
+    # Open raster band
+    raster_band = osgeotools.open_raster_band(resampled_dem, 1, True)
+
+    # Check if output directory exists
+    out_dir = osgeotools.isexists(out_dir)
+
+    # Generate tile upper left coordinates
+    _logger.info("Generating tiles...")
+    tile_counter = 0
+    for tile_y in xrange(tile_extents["min_y"] + _TILE_SIZE,
+                         tile_extents["max_y"] + _TILE_SIZE,
+                         _TILE_SIZE):
+        for tile_x in xrange(tile_extents["min_x"],
+                             tile_extents["max_x"],
+                             _TILE_SIZE):
+
+            # Get tile of band array
+            tile_data = osgeotools.get_band_array_tile(resampled_dem,
+                                                       raster_band,
+                                                       tile_x, tile_y,
+                                                       _TILE_SIZE)
+
+            # If tile has data
+            if not tile_data is None:
+                ctr = 0
+                tile, ul_x, ul_y = tile_data
+
+                # Create new tile geotransform
+                tile_gt = list(dem["geotransform"])
+                tile_gt[0], tile_gt[3] = ul_x, ul_y
+
+                # Construct filename
+                filename = "E%sN%s_%s_%s.tif" % (tile_x / _TILE_SIZE,
+                                                 tile_y / _TILE_SIZE,
+                                                 dem_type.upper(),
+                                                 rbid.upper())
+                tile_path = os.path.join(out_dir, filename)
+
+                # Check if output filename is already exists
+                while os.path.exists(tile_path):
+                    print '\nWARNING:', tile_path, 'already exists'
+                    ctr += 1
+                    filename = filename.replace(
+                        #'.tif', '_' + str(ctr) + '_PL1' + '.tif')
+                        '.tif', '_' + str(ctr) + '.tif')
+                    tile_path = os.path.join(out_dir, filename)
+
+                # Save new GeoTIFF
+                osgeotools.write_raster(tile_path, "GTiff", tile,
+                                        osgeotools.gdalconst.GDT_Float32,
+                                        tile_gt, dem, raster_band)
+                _logger.info(src_dem + ' --------- ' +
+                             filename + ' --------- ')
+
+            tile_counter += 1
+
+        # exit(1)
+    _logger.info("Total no. of tiles: {0}".format(tile_counter))
+
+    # Delete temporary resampled DEM
+    resampled_dem = None
+    os.remove(resampled_dem_path)
+
 
 if __name__ == '__main__':
 
@@ -186,7 +338,8 @@ if __name__ == '__main__':
                     print '\nWARNING:', tile_path, 'already exists'
                     ctr += 1
                     filename = filename.replace(
-                        '.tif', '_' + str(ctr) + '_PL1' + '.tif')
+                        #'.tif', '_' + str(ctr) + '_PL1' + '.tif')
+                        '.tif', '_' + str(ctr) + '.tif')
                     tile_path = os.path.join(output_dir, filename)
 
                 # Save new GeoTIFF
