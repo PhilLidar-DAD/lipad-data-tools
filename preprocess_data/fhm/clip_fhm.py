82860c0eaf658892c33ea5a76d765a028bcc64c0
Jok Laurente <jalaurente@dream.upd.edu.ph>
2017-05-08 17:32:09 +0800

    see description for changelog
    - replaced textfile log by csvfile log
    - stored intermediate data to "in_memory"
    - added function for identifying fhm with 80% coverage and built-up
    areas

--- preprocess_data/fhm/clip_fhm.py
+++ preprocess_data/fhm/clip_fhm.py
@@ -1,14 +1,15 @@
 # Windows
 # ArcPy
 
-__version__ = "0.5.1"
+__version__ = "0.6"
 
 import arcpy
 import os
 import sys
 import time
 import argparse
-from datetime import datetime
+import csv
+from datetime import datetime as dt
 
 parser = argparse.ArgumentParser(description='Clip flood hazard maps per municipality')
 parser.add_argument('-i','--input_directory')
@@ -17,7 +18,7 @@ args = parser.parse_args()
 print "Python version:", sys.version
 print "Script version:", __version__
 
-print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 "Modules were successfully imported"
 
 startTime = time.time()
@@ -25,26 +26,31 @@ startTime = time.time()
 arcpy.env.outputZFlag = "Disabled"
 arcpy.env.outputMFlag = "Disabled"
 
-current_date = str(datetime.now()).split(" ",1)[0]
-log_file = open("logs_" + current_date + ".txt", "a")
+if os.path.exists("clip_fhm_logs.csv"):
+	csvfile = open("clip_fhm_logs.csv", 'ab')
+	spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+else:
+	csvfile = open("clip_fhm_logs.csv", 'ab')
+	spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+	spamwriter.writerow(['Riverbasin/Floodplain', 'Return Period','Province', 'City/Municipality','Geocode','Status', 'Timestamp'])
 
 # define variables
 # replace output directory
 input_directory = args.input_directory
-output_directory = r"E:\FLOOD_HAZARD_MAPS\Testing\Clipped_FHM"
-muni_boundary = r"E:\FLOOD_HAZARD_MAPS\Testing\Clip_FHM\FHM_Index.gdb\PSA_Muni_Boundary"
+output_directory = r"\\pmsat-nas.prd.dream.upd.edu.ph\geostorage\DAD\FLOOD_HAZARD\Clipped_FHMs"
+muni_boundary = r"\\pmsat-nas.prd.dream.upd.edu.ph\geostorage\DAD\FLOOD_HAZARD\FHM_Monitoring.gdb\PSA_Muni_Boundary"
+muni_index= r"\\pmsat-nas.prd.dream.upd.edu.ph\geostorage\DAD\FLOOD_HAZARD\FHM_Monitoring.gdb\PSA_Muni_Index"
 
 # muni boundary fields
 muni_fields = ['REG_NAME', 'PRO_NAME', 'MUN_NAME', 'MUN_CODE', 'RB_FP', 'Return_Period', \
-'Last_Clipped', 'Resolution', 'SHAPE@']
+'Last_Clipped', 'Resolution', 'FHM_80_coverage', 'Builtup_coverage', 'Status', 'SHAPE@']
 
 # temporary shapefiles
-temp_erase = os.path.join(output_directory,"temp_erase.shp")
-temp_select = os.path.join(output_directory,"temp_select.shp")
-temp_fhm = os.path.join(output_directory,"temp_fhm.shp")
-temp_duplicate = os.path.join(output_directory,"temp_duplicate.shp")
-temp_dissolve = os.path.join(output_directory,"temp_dissolve.shp")
-temp_intersect = os.path.join(output_directory,"temp_intersect.shp")
+erase = r"in_memory\temp_erase"
+select = r"in_memory\temp_select"
+temp_fhm = r"in_memory\temp_fhm"
+duplicate = r"in_memory\temp_duplicate"
+table = r"in_memory\temp_table"
 
 # layers
 muni_layer = "muni_layer"
@@ -54,12 +60,13 @@ temp_fhm_layer = "temp_fhm_layer"
 fhm_list_ok = []
 fhm_list_err = []
 
-arcpy.MakeFeatureLayer_management(muni_boundary, muni_layer, "", "", \
+arcpy.MakeFeatureLayer_management(muni_index, muni_layer, "", "", \
 				"OBJECTID_1 OBJECTID_1 VISIBLE NONE;Shape Shape VISIBLE NONE;REG_CODE REG_CODE \
 				VISIBLE NONE;REG_NAME REG_NAME VISIBLE NONE;PRO_CODE PRO_CODE VISIBLE NONE;PRO_NAME \
 				PRO_NAME VISIBLE NONE;MUN_CODE MUN_CODE VISIBLE NONE;MUN_NAME MUN_NAME VISIBLE \
-				NONE;ISCITY ISCITY VISIBLE NONE;Shape_Length Shape_Length VISIBLE NONE;Shape_Area \
-				Shape_Area VISIBLE NONE")
+				NONE;ISCITY ISCITY VISIBLE NONE;FHM_80_coverage FHM_80_coverage VISIBLE NONE;\
+				Builtup_coverage Builtup_coverage VISIBLE NONE;Status Status VISIBLE NONE;\
+				Shape_Length Shape_Length VISIBLE NONE;Shape_Area Shape_Area VISIBLE NONE")
 
 # loop through the flood hazard shapefiles
 for path, dirs, files in os.walk(input_directory,topdown=False):
@@ -67,42 +74,44 @@ for path, dirs, files in os.walk(input_directory,topdown=False):
 		if f.endswith(".shp"):
 			print "\n" + "#" * 70 + "\n"
 			try:
+				for_checking_value = True
 				fhm_path = os.path.join(path,f)
 				rbfp_name = f.split("_FH")[0]
 				year = f.split("_FH")[1].split("yr")[0]
 				resolution = f.split("_FH")[1].split("yr_")[1].replace(".shp","")
-				print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+				print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 				"FHM path:", fhm_path
-				print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+				print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 				"Riverbasin/Floodplain name:", rbfp_name
-				print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+				print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 				"Return period:", year
-				print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+				print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 				"Resolution:", resolution
 
-				print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+				print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 				"Selecting municipalities intersected with fhm"
 				arcpy.SelectLayerByLocation_management(muni_layer, "INTERSECT", fhm_path,\
-				 "", "NEW_SELECTION", "NOT_INVERT")
+				 "", "NEW_SELECTION")
 
-				################### fhm coverage #####################
-				
-				print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
-				"Dissolving fhm"
-				arcpy.Dissolve_management(fhm, temp_dissolve)
+				print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+				"Performing analysis on selected municipalities"
+				arcpy.Statistics_analysis(muni_layer, table, [["FHM_80_coverage", "SUM"], ["Builtup_coverage", "SUM"]])
+				table_fields = ["SUM_FHM_80_coverage", "SUM_Builtup_coverage"]
+				cursor1 = arcpy.da.SearchCursor(table, table_fields)
+				for row1 in cursor1:
+					print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+					"Number of city/muni with 80% FHM coverage: ", str(row1[0])
+					print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+					"Number of city/muni with built-up areas: ", str(row1[1])
 
-				print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
-				"Adding fields to dissolved fhm"
-				arcpy.AddField_management(temp_dissolve, "RBFP_shp", "TEXT")
-				arcpy.AddField_management(temp_dissolve, "RBFP_name", "TEXT")
-				arcpy.AddField_management(temp_dissolve, "Processor", "TEXT")
+					if row1[0] > 0:
+						for_checking_value = False
+					if row1[1] > 0:
+						for_checking_value = False
+					print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:",\
+					"FHM for checking: " , for_checking_value
 
-				print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
-				"Calculating field of dissolved fhm "
-				arcpy.CalculateField_management(temp_dissolve, "RBFP_shp",'"' + rbfp_name + '"', "PYTHON_9.3")
-
-				################### fhm coverage #####################
-
+				# loop through the selected muni
 				cursor = arcpy.da.UpdateCursor(muni_layer, muni_fields)
 				for row in cursor:
 					fhm_exists = False
@@ -110,115 +119,200 @@ for path, dirs, files in os.walk(input_directory,topdown=False):
 					province = row[1]
 					muni = row[2]
 					geocode = row[3]
-					geom = row[8]
+					geom = row[11]
+
+					coverage_80 = False
+					builtup = False
+
+					if row[8] == 1:
+						coverage_80 = True
+					if row[9] == 1:
+						builtup = True
 
 					print "\n" + "-" * 70 + "\n"
 
-					arcpy.Intersect_analysis([temp_dissolve,geom], temp_intersect)
-					arcpy.AddField_management(temp_intersect, "Area_hazard", "DOUBLE")
-					
+					print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+					"Region:", region
+					print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+					"Province:", province
+					print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+					"City/Municipality:", muni
+					print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+					"80% Coverage:", coverage_80
+					print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+					"Builtup COVERAGE:", builtup
 
-					output_path = os.path.join(output_directory, region, province, muni)
+					output_path_archive = os.path.join(output_directory, "FOR_ARCHIVING", region, province, muni)
+					output_path_upload = os.path.join(output_directory, "FOR_UPLOADING", region, province, muni)
 
-					if not os.path.exists(output_path):
-						print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+					if not os.path.exists(output_path_archive):
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 						"Creating output directories"
-						os.makedirs(output_path)
+						os.makedirs(output_path_archive)
 					else:
-						print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 						"Output directory already exists"
 
-					output_fhm = os.path.join(output_path, geocode + "_FH" + year + "yr"\
+					output_fhm = os.path.join(output_path_archive, geocode + "_FH" + year + "yr"\
 					+ "_" + resolution + ".shp")
 
-					print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+					print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 						"FHM output:", output_fhm
 
 					# check if fhm exists
 					if os.path.exists(output_fhm):
-						print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 						"Flood hazard map already exists"
 						fhm_exists = True
 
 					if not fhm_exists:
 						# clip flood hazard shapefile using muni index as extent
-						print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 						"Clipping flood hazard shapefile"
 						arcpy.Clip_analysis(fhm_path, geom, temp_fhm, "")
 
 						# erase the muni index extent using fhm to generate "area not assessed"
-						print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 						"Erasing muni index"
-						arcpy.Erase_analysis(geom, temp_fhm, temp_erase)
+						arcpy.Erase_analysis(geom, temp_fhm, erase)
 
 						# add fields to the temporary "area not assessed" shapefile
-						print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 						"Adding Var field to temporary 'area not assessed' shapefile"
-						arcpy.AddField_management(temp_erase, "Var", "SHORT")
+						arcpy.AddField_management(erase, "Var", "SHORT")
 
-						# arcpy.AddField_management(temp_erase, "Muncode", "TEXT")
-						print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
-						"Calculating temp_erase's Var field"
-						arcpy.CalculateField_management(temp_erase, "Var", "-1","PYTHON_9.3")
+						# calculate value of the temporary "area not assessed" shapefile 
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						"Calculating erase's Var field"
+						arcpy.CalculateField_management(erase, "Var", "-1","PYTHON_9.3")
 
-						# append the "area not assessed" to fhm output
-						print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						# append the temporary "area not assessed" to fhm output
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 						"Appending 'area not assessed' to fhm output"
-						arcpy.Append_management(temp_erase, temp_fhm, "NO_TEST")
+						arcpy.Append_management(erase, temp_fhm, "NO_TEST")
+
+						# generate temporary municipal outline
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						"Generating municipal outline"
+						where_clause = "{0} = '{1}'".format('"MUN_CODE"', geocode)
+						arcpy.Select_analysis(muni_boundary, select, where_clause)
+
+						# add fields to the municpal outline shapefile
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						"Adding Var field to temporary municipal outline shapefile"
+						arcpy.AddField_management(select, "Var", "SHORT")
+
+						# calculate value of the temporary municipal outline shapefile 
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						"Calculating select's Var field"
+						arcpy.CalculateField_management(select, "Var", "-2","PYTHON_9.3")
+
+						# append the municipal outline to fhm output
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						"Appending municipal outline to fhm output"
+						arcpy.Append_management(select, temp_fhm, "NO_TEST")
 
 						# dissolve the fhm
-						print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 						"Dissolving fhm output"
 						arcpy.Dissolve_management (temp_fhm, output_fhm, "Var")
 
 					else:
 
-						# create a duplicate copy of the old fhm
-						arcpy.CopyFeatures_management(output_fhm, temp_duplicate)
-						print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
-						"Creating a duplicate copy of the old fhm"
+						# create a duplicate copy of the old fhm except for municipal outline
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						"Creating a duplicate copy of the old fhm except for municipal outline"
+						where_clause = "{0} <> {1}".format('"Var"', "-2")
+						arcpy.Select_analysis(output_fhm, duplicate, where_clause)
 
 						# delete the old fhm
 						arcpy.Delete_management(output_fhm)
-						print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 						"Deleting the old fhm"
 
 						# clip flood hazard shapefile using muni index as extent
-						print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 						"Clipping flood hazard shapefile"
 						arcpy.Clip_analysis(fhm_path, geom, temp_fhm, "")
 
 						# erase the duplicate fhm using clipped fhm"
-						print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 						"Erasing muni index"
-						arcpy.Erase_analysis(temp_duplicate, temp_fhm, temp_erase)
+						arcpy.Erase_analysis(duplicate, temp_fhm, erase)
 
 						# append the erased fhm to to fhm output
-						print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 						"Appending 'area not assessed' to fhm output"
-						arcpy.Append_management(temp_erase, temp_fhm, "NO_TEST")
+						arcpy.Append_management(erase, temp_fhm, "NO_TEST")
+
+						##########################
+						# generate temporary municipal outline
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						"Generating municipal outline"
+						where_clause = "{0} = '{1}'".format('"MUN_CODE"', geocode)
+						arcpy.Select_analysis(muni_boundary, select, where_clause)
+
+						# add fields to the municpal outline shapefile
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						"Adding Var field to temporary municipal outline shapefile"
+						arcpy.AddField_management(select, "Var", "SHORT")
+
+						# calculate value of the temporary municipal outline shapefile 
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						"Calculating select's Var field"
+						arcpy.CalculateField_management(select, "Var", "-2","PYTHON_9.3")
+
+						# append the municipal outline to fhm output
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						"Appending municipal outline to fhm output"
+						arcpy.Append_management(select, temp_fhm, "NO_TEST")
+						##########################
 
 						# dissolve the fhm
-						print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 						"Dissolving fhm output"
 						arcpy.Dissolve_management (temp_fhm, output_fhm, "Var")
 
-					print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+					print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 					"Adding Muncode field to fhm output"
 					arcpy.AddField_management(output_fhm, "Muncode", "TEXT")
 
-					print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+					print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 					"Calculating output fhm's Muncode"
 					arcpy.CalculateField_management(output_fhm, "Muncode", '"' + geocode + '"', \
 						"PYTHON_9.3")
 
-					print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+					print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
 					"Updating spatial index"
 					arcpy.AddSpatialIndex_management(output_fhm)
 
-					# update index psa after clipping
+					# check if fhm output is for uploading
+					if coverage_80 or builtup:
+						output_fhm_upload = output_fhm.replace("FOR_ARCHIVING","FOR_UPLOADING")
+						if not os.path.exists(output_path_upload):
+							print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+							"Creating output directories"
+							os.makedirs(output_path_upload)
+						else:
+							print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+							"Output directory already exists"
+						
+						if os.path.exists(output_fhm_upload):
+							print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+							"Deleting the old fhm"
+							arcpy.Delete_management(output_fhm_upload)
 
-					# Riverbasin/Floodplain name
+					 	print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+						"Copying output fhm to FOR_UPLOADING folder"
+						arcpy.CopyFeatures_management(output_fhm, output_fhm_upload)
+
+						row[10] = "FOR_UPLOADING"
+
+					elif for_checking_value:
+						row[10] = "FOR_CHECKING"
+					else:
+						row[10] = "FOR_ARCHIVING"
+
 					# check if field is null
 					if row[4] is None:
 						row[4] = rbfp_name
@@ -245,37 +339,20 @@ for path, dirs, files in os.walk(input_directory,topdown=False):
 					else:
 						row[7] = row[7] + ", " + resolution
 
-					row[6] = datetime.now()
+					row[6] = dt.now()
 					cursor.updateRow(row)
 
+					spamwriter.writerow([rbfp_name, year, province, muni, geocode, row[10], dt.now().strftime('%Y-%m-%d %H:%M:%S')])
+
 					# deleting temporary files
-					print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
-					"Deleting temporary shapefiles"
-					arcpy.Delete_management(temp_erase)
-					arcpy.Delete_management(temp_select)
-					arcpy.Delete_management(temp_fhm)
-					arcpy.Delete_management(temp_duplicate)
-					arcpy.Delete_management(temp_fhm_layer)
+					print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", \
+					"Deleting in_memory workspace"
+					arcpy.Delete_management("in_memory")
 
-				fhm_list_ok.append(fhm_path)
 			except Exception, e:
-				print "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", e
-				fhm_list_err.append(fhm_path)
-
-print "List of flood hazard maps clipped"
-log_file.write("List of flood hazard maps clipped\n")
-
-for fhm_ok in fhm_list_ok:
-	print fhm_ok
-	log_file.write(fhm_ok + "\n")
-
-print "\nList of flood hazard maps with error\n"
-log_file.write("List of flood hazard maps with error\n")
-
-for fhm_err in fhm_list_err:
-	print fhm_err
-	log_file.write(fhm_err + "\n")
-
-log_file.close()
+				print "[" + dt.now().strftime('%Y-%m-%d %H:%M:%S') + "]:", str(e)
+				spamwriter.writerow([rbfp_name, "", "", "", "", "ERROR:", dt.now().strftime('%Y-%m-%d %H:%M:%S')])
+			
+csvfile.close()
 endTime = time.time()  # End timing
 print '\nElapsed Time:', str("{0:.2f}".format(round(endTime - startTime,2))), 'seconds'
