7905cc86917eae0cad1415ba4acbb2932f703364
Denise Leviste <drleviste@gmail.com>
2017-05-08 15:24:21 +0800

    LAZ, ortho, metadata automation folder scripts

---
+++ automation/trigger.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import peewee
+from settings import *
+
+
+PSQL_DB = peewee.PostgresqlDatabase(
+    DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+
+
+class BaseModel(peewee.Model):
+
+    class Meta:
+        database = PSQL_DB
+
+
+class Automation_AutomationJob(BaseModel):
+    datatype = peewee.CharField(max_length=10)
+    input_dir = peewee.CharField(max_length=255, null=False)
+    output_dir = peewee.CharField(max_length=255)
+    processor = peewee.CharField(max_length=10)
+    date_submitted = peewee.DateTimeField(null=False)
+    status = peewee.CharField(max_length=20)
+    target_os = peewee.CharField(max_length=20)
+    log = peewee.TextField(null=False)
+
+    class Meta:
+        primary_key = peewee.CompositeKey(
+            'datatype', 'date_submitted', 'status')
+
