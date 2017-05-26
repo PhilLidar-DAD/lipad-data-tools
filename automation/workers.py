85c2eabf6df6a28b18c3797ee8c90c36c105f59f
Ken Salanio <kssalanio@gmail.com>
2017-05-26 14:03:05 +0800

    added handler for DEM tiling

--- automation/workers.py
+++ automation/workers.py
@@ -2,6 +2,7 @@ from models import *
 from rename_tiles import *
 from transfer_metadata import *
 from utils import *
+from exceptions import *
 import os
 import sys
 import logging
@@ -9,6 +10,8 @@ import random
 import time
 import subprocess
 from datetime import datetime
+import json as py_json
+
 
 logger = logging.getLogger()
 LOG_LEVEL = logging.DEBUG
@@ -82,7 +85,7 @@ def process_job(q):
             if not files_renamed(input_dir):
                 rename_tiles(input_dir, output_dir, processor, block_uid)
         else:
-            print 'DTM datatype'
+            raise Exception('Handler not implemented for type: '+str(q.datatype))
 
         assign_status(q, 2)
         print 'Status', q.status
@@ -95,6 +98,69 @@ def process_job(q):
 
     else:
         print 'ERROR NOT FOUND IN MODEL', block_name, block_uid
+
+def parse_dem_input(input_str):
+    tokens = input_str.strip().replace(' ', '').split(',')
+    block_names = tokens[1:]
+    dem_dir = os.path.join("/mnt/pmsat_pool/geostorage/DPC/",tokens[0].replace('\\', '/').split('DPC/')[1])
+    
+    #if not os.path.isdir(dem_dir):
+    if False:
+        raise DirectoryNotFoundException("Directory does not exist: "+dem_dir)
+    
+    return dem_dir, block_names
+
+def handle_dem(q):
+    assign_status(q, 1)
+    print 'Status', q.status
+    print 'Status Timestamp', q.status_timestamp
+    print 'Processing Job'
+    input_dir = q.input_dir
+    output_dir = q.output_dir
+    processor = q.processor
+    
+    dem_dir, block_name_list = parse_dem_input(input_dir)
+    
+    print 'BLOCKS: ' + str(block_name_list)
+    
+    #Convert block_names to block_uids
+    block_uid_list = []
+    for block_name in block_name_list:
+        in_coverage, block_uid = find_in_coverage(block_name)
+        block_uid_list.append(tuple(block_uid, in_coverage))
+        
+    block_uid_list_json = py_json.dumps(block_uid_list)
+    
+    """
+        @TODO:
+        1) Tile blocks for each metadata
+        2) Pass to LiPAD db
+        3) Talk to DJ about file and metadata naming conventions
+    """
+    
+    if q.datatype.lower == 'DTM':
+        """Tile DTM only"""
+        tile_dtm(dem_dir, output_dir)
+    elif q.datatype.lower == 'DSM':
+        """Tile DSM only"""
+        tile_dsm(dem_dir, output_dir)
+    elif q.datatype.lower == 'DEM' or q.datatype.lower == 'DTM/DSM' :
+        """Tile both DEMS"""
+        
+    """
+        @TODO:
+        1) Upload tiles
+        2) Pass CephDataObject metadata to LiPAD db
+    """
+    assign_status(q, 2)
+    print 'Status', q.status
+    print 'Status Timestamp', q.status_timestamp
+    ceph_uploaded, log_file = ceph_upload(output_dir)
+
+    if ceph_uploaded:
+        assign_status(q, 3)
+        transfer_metadata(log_file)
+
 
 
 def db_watcher():
