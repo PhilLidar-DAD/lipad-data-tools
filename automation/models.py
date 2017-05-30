3ef226e2b014c5e7b5c13e68b03978f55564ae95
Ken Salanio <kssalanio@gmail.com>
2017-05-30 15:06:29 +0800

    resolved merge conflict on automation/models.py

--- automation/models.py
+++ automation/models.py
@@ -15,65 +15,16 @@ class BaseModel(peewee.Model):
         database = PSQL_DB
 
 
-class DataClassification(Field):
-    UNKNOWN = 0
-    LAZ = 1
-#    DEM = 2
-    DTM = 3
-    DSM = 4
-    ORTHOPHOTO = 5
-
-    labels = {
-        UNKNOWN: "Unknown Type",
-        LAZ: "LAZ",
-        #        DEM         : "DEM TIF",
-        DSM: "DSM TIF",
-        DTM: "DTM TIF",
-        ORTHOPHOTO: "Orthophoto", }
-
-    gs_feature_labels = {
-        UNKNOWN: "UNSUPPORTED",
-        LAZ: "LAZ",
-        #        DEM         : "UNSUPPORTED",
-        DSM: "DSM",
-        DTM: "DTM",
-        ORTHOPHOTO: "ORTHO", }
-
-    filename_suffixes = {
-        ".laz": LAZ,
-        #        "_dem.tif"         : DEM,
-        "_dsm.tif": DSM,
-        "_dtm.tif": DTM,
-        "_ortho.tif": ORTHOPHOTO, }
-
-
 class Automation_AutomationJob(BaseModel):
-
-    STATUS_CHOICES = [
-        ('pending_process'),
-        ('done_process'),
-        ('pending_ceph'),
-        ('done_ceph'),
-        ('done'),
-        # (-1, 'error', _('Error')),
-    ]
-
-    OS_CHOICES = [
-        ('linux', ('Process in Linux')),
-        ('windows', ('Process in Windows')),
-    ]
-
     id = peewee.IntegerField(primary_key=True)
     datatype = peewee.CharField(max_length=10)
     input_dir = peewee.TextField(null=False)
     output_dir = peewee.TextField(null=False)
     processor = peewee.CharField(max_length=10)
     date_submitted = peewee.DateTimeField(null=False)
-    # status = peewee.CharField(max_length=20)
-    status = peewee.CharField(choices=STATUS_CHOICES)
+    status = peewee.CharField(max_length=20)
     status_timestamp = peewee.DateTimeField(null=True)
-    target_os = peewee.CharField(choices=OS_CHOICES)
-    # target_os = OSChoice(choices=OS_CHOICES)
+    target_os = peewee.CharField(max_length=20)
     log = peewee.TextField(null=False)
 
     # class Meta:
@@ -132,7 +83,7 @@ class DataClassification(Field):
         "_dtm.tif": DTM,
         "_ortho.tif": ORTHOPHOTO, }
 
-class Cephgeo_DemDataStore(BaseModel):
+class DemDataStore(BaseModel):
     demid   = peewee.IntegerField(primary_key=True)
     name    = peewee.CharField(max_length=20)
     suc     = peewee.CharField(max_length=5)
@@ -140,7 +91,7 @@ class Cephgeo_DemDataStore(BaseModel):
     shifting_val_x = peewee.FloatField() 
     shifting_val_y = peewee.FloatField() 
     shifting_val_z = peewee.FloatField()
-    height_diff = peewee.FloatField()
-    rmse = peewee.FloatField()
-    dem_file_path = peewee.TextField(null=False)
+    height_diff    = peewee.FloatField()
+    rmse           = peewee.FloatField()
+    dem_file_path  = peewee.TextField(null=False)
     block_name_list = peewee.TextField(null=False)
