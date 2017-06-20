384fc2c75727ca24f7a94242a7dd182434224b16
Ken Salanio <kssalanio@gmail.com>
2017-06-20 11:30:46 +0800

    revised model for LiPAD DemDataStore

--- automation/models.py
+++ automation/models.py
@@ -15,59 +15,6 @@ class BaseModel(peewee.Model):
         database = PSQL_DB
 
 
-class Automation_AutomationJob(BaseModel):
-    id = peewee.IntegerField(primary_key=True)
-    datatype = peewee.CharField(max_length=10)
-    input_dir = peewee.TextField(null=False)
-    output_dir = peewee.TextField(null=False)
-    processor = peewee.CharField(max_length=10)
-    date_submitted = peewee.DateTimeField(null=False)
-    status = peewee.CharField(max_length=20)
-    status_timestamp = peewee.DateTimeField(null=True)
-    target_os = peewee.CharField(max_length=20)
-    log = peewee.TextField(null=False)
-
-    # class Meta:
-    #     primary_key = peewee.CompositeKey(
-    #         'datatype', 'date_submitted', 'status')
-
-
-class Cephgeo_LidarCoverageBlock(BaseModel):
-    """
-        From geonode.cephgeo.models LidarCoverageBlock
-        Only UID and Block Name needed for renaming tiles
-    """
-    adjusted_l = TextField()
-    block_name = CharField(index=True)
-    cal_ref_pt = TextField()
-    date_flown = DateField(null=True)
-    flight_num = TextField()
-    floodplain = TextField()
-    height_dif = TextField()
-    mission_na = TextField()
-    pl1_suc = TextField()
-    pl2_suc = TextField()
-    processor = TextField()
-    rmse_val_m = TextField()
-    sensor = TextField()
-    uid = PrimaryKeyField()
-    val_ref_pt = TextField()
-    x_shift_m = TextField()
-    y_shift_m = TextField()
-    z_shift_m = TextField()
-
-
-class CephDataObject(BaseModel):
-    id = peewee.IntegerField(primary_key=True)
-    size_in_bytes = peewee.IntegerField()
-    file_hash = peewee.CharField(max_length=40)
-    name = peewee.CharField(max_length=100)
-    last_modified = peewee.DateTimeField()
-    content_type = peewee.CharField(max_length=20)
-    data_class = peewee.CharField(max_length=20)
-    grid_ref = peewee.CharField(max_length=10)
-
-
 class DataClassification(Field):
     UNKNOWN = 0
     LAZ = 1
@@ -99,10 +46,44 @@ class DataClassification(Field):
         "_dtm.tif": DTM,
         "_ortho.tif": ORTHOPHOTO, }
 
+
+class Automation_AutomationJob(BaseModel):
+
+    STATUS_CHOICES = [
+        ('pending_process'),
+        ('done_process'),
+        ('pending_ceph'),
+        ('done_ceph'),
+        ('done'),
+        # (-1, 'error', _('Error')),
+    ]
+
+    OS_CHOICES = [
+        ('linux', ('Process in Linux')),
+        ('windows', ('Process in Windows')),
+    ]
+
+    id = peewee.IntegerField(primary_key=True)
+    datatype = peewee.CharField(max_length=10)
+    input_dir = peewee.TextField(null=False)
+    output_dir = peewee.TextField(null=False)
+    processor = peewee.CharField(max_length=10)
+    date_submitted = peewee.DateTimeField(null=False)
+    # status = peewee.CharField(max_length=20)
+    status = peewee.CharField(choices=STATUS_CHOICES)
+    status_timestamp = peewee.DateTimeField(null=True)
+    target_os = peewee.CharField(choices=OS_CHOICES)
+    # target_os = OSChoice(choices=OS_CHOICES)
+    log = peewee.TextField(null=False)
+
+    # class Meta:
+    #     primary_key = peewee.CompositeKey(
+    #         'datatype', 'date_submitted', 'status')
+
 class Automation_Demdatastore(BaseModel):
-    demid = PrimaryKeyField()
     block_name_list = TextField()
     dem_file_path = TextField()
+    demid = PrimaryKeyField()
     height_diff = FloatField()
     name = CharField()
     rmse = FloatField()
@@ -111,3 +92,56 @@ class Automation_Demdatastore(BaseModel):
     shifting_val_z = FloatField()
     suc = CharField()
     type = CharField()
+
+    class Meta:
+        db_table = 'automation_demdatastore'
+
+class Cephgeo_LidarCoverageBlock(BaseModel):
+    """
+        From geonode.cephgeo.models LidarCoverageBlock
+        Only UID and Block Name needed for renaming tiles
+    """
+    adjusted_l = TextField()
+    block_name = CharField(index=True)
+    cal_ref_pt = TextField()
+    date_flown = DateField(null=True)
+    flight_num = TextField()
+    floodplain = TextField()
+    height_dif = TextField()
+    mission_na = TextField()
+    pl1_suc = TextField()
+    pl2_suc = TextField()
+    processor = TextField()
+    rmse_val_m = TextField()
+    sensor = TextField()
+    uid = PrimaryKeyField()
+    val_ref_pt = TextField()
+    x_shift_m = TextField()
+    y_shift_m = TextField()
+    z_shift_m = TextField()
+
+    class Meta:
+        db_table = 'cephgeo_lidarcoverageblock'
+
+
+class CephDataObject(BaseModel):
+    id = peewee.IntegerField(primary_key=True)
+    size_in_bytes = peewee.IntegerField()
+    file_hash = peewee.CharField(max_length=40)
+    name = peewee.CharField(max_length=100)
+    last_modified = peewee.DateTimeField()
+    content_type = peewee.CharField(max_length=20)
+    data_class = peewee.CharField(max_length=20)
+    grid_ref = peewee.CharField(max_length=10)
+
+
+class Cephgeo_DemDataStore():
+    demid = PrimaryKeyField()
+    name = CharField()
+    type = CharField()
+    dem_file_path = TextField(null=False)
+    cephdataobject = ForeignKeyField(db_column='cephdataobject_id', rel_model=CephDataObject, to_field='id')
+    lidar_block = ForeignKeyField(db_column='lidar_block_id', rel_model=Cephgeo_LidarCoverageBlock, to_field='uid')
+    
+    class Meta:
+        db_table = 'automation_demdatastore'
