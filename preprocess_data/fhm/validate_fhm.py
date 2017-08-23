49bdbb11d4e2698bd5ee0cbc9bce500d99fa8df9
Jok Laurente <jalaurente@dream.upd.edu.ph>
2017-08-23 15:00:05 +0800

    check if schema is valid

--- preprocess_data/fhm/validate_fhm.py
+++ preprocess_data/fhm/validate_fhm.py
@@ -1,4 +1,4 @@
-__version__ = "0.1"
+__version__ = "0.2"
 __authors__ = "Jok Laurente"
 __email__ = ["jmelaurente@gmail.com"]
 __description__ = 'Validation of Flood Hazard Maps'
@@ -29,13 +29,31 @@ parser.add_argument('-i','--input_directory')
 args = parser.parse_args()
 
 input_directory = args.input_directory
-csvfile = open("validate_fhm.csv", 'ab')
-spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+csv_file = open("validate_fhm.csv", 'wb')
+spamwriter = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+spamwriter.writerow(['Filename', 'Path', 'SRID', 'Schema'])
 
 for path, dirs, files in os.walk(input_directory, topdown=False):
 	for f in files:
 		if f.endswith(".shp"):
+			var_field = False
 			fhm = os.path.join(path,f)
+
+			# check spatial reference
 			logger.info("%s: Checking spatial reference", f)
-			srs = arcpy.Describe(fhm).spatialReference.PCSCode
-			logger.info("{0}: {1}".format(f,srs))
+			srid = arcpy.Describe(fhm).spatialReference.PCSCode
+			logger.info("{0}: SRID: {1}".format(f,srid))
+
+			# check schema
+			logger.info("%s: Checking if Var field exists", f)
+			fields = arcpy.ListFields(fhm)
+			for field in fields:
+				if field.name == 'Var':
+					var_field = True
+					break
+			if var_field:
+				logger.info("%s: Var field exists", f)
+			else:
+				logger.info("%s: Var field doesn't exists", f)
+			spamwriter.writerow([f, path, srid, var_field])
+csv_file.close()
